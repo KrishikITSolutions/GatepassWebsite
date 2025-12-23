7fb9cead3a8ca173c56187bec6aa42f0ef451b6b
subbaramaiah <ram.t@krishikit.ai>
2025-12-23 14:22:13 +0530

    pushing-code

--- app/api/send-otp/route.ts
+++ app/api/send-otp/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import { supabase } from "@/app/utils/supabase";
 import { otpStore } from "@/app/utils/otpStore"; // in-memory or replace with Redis
 import twilio from "twilio";
